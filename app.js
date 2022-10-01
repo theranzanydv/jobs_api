aaf870ff87ccc602c1feb70d7cb2bba81170f9d4
Ranjan Yadav <theranzanydv@gmail.com>
2022-10-01 22:08:16 +0545

    Swagger UI added

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const cors = require('cors')
 const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit')
 
+// Swagger
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+
+const swaggerDocument = YAML.load('./swagger.yaml')
+
 const express = require('express')
 const app = express()
 
@@ -41,8 +47,10 @@ app.use(xss())
 
 // routes
 app.get('/', (req, res) => {
-	res.send('Welcome to the Job Portal API')
+	res.send('<h1>Jobs Api</h1><a href="/api-docs">Documentation</a>')
 })
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
